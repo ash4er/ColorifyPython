ea29fc17827f0f999bde2203d7a73c55b8fef3f6
ash4er <ash4er@gmail.com>
2017-09-12 16:34:33 +0300

    Tab added to a string

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 from colors import cprint 
 
-cprint("Test string", "gray")
-cprint("Test string", "red")
-cprint("Test string", "green")
-cprint("Test string", "yellow")
-cprint("Test string", "blue")
-cprint("Test string", "magenta")
-cprint("Test string", "cyan")
-cprint("Test string", "white")
+cprint("\tTest string", "gray")
+cprint("\tTest string", "red")
+cprint("\tTest string", "green")
+cprint("\tTest string", "yellow")
+cprint("\tTest string", "blue")
+cprint("\tTest string", "magenta")
+cprint("\tTest string", "cyan")
+cprint("\tTest string", "white")
